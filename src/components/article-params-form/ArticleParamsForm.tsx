59b17c9686aea62c4f80da0c86231a655ed2f585
Roman Maslennikov <romzes120282@gmail.com>
2024-06-04 18:27:42 +0300

    реализация ArrowButton.tsx

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,28 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Text } from 'components/text';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setOpen] = useState(false);
+	const toggleShowForm = () => {
+		setOpen(!isOpen);
+		console.log(`form is open =  ${isOpen}`);
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isActive={isOpen} onClick={toggleShowForm} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<Text uppercase={true} weight={800} size={31}>
+						Задайте параметры
+					</Text>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
